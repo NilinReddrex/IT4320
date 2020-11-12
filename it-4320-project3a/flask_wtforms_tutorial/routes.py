b1944d50a0721de32c5a9cdb42e053d64f6e4b03
Nilin Reddrex <nilin.reddrex@outlook.com>
2020-11-11 20:27:50 -0800

    Project3A initial commit.

---
+++ it-4320-project3a/flask_wtforms_tutorial/routes.py
@@ -0,0 +1,64 @@
+from flask import current_app as app
+from flask import redirect, render_template, url_for, request, flash, session
+
+from .forms import StockForm
+from .charts import *
+from .symbolInput import SymbolInput
+from .chartTypeInput import ChartTypeInput
+from .timeSeriesInput import TimeSeriesInput
+from .dateInput import DateInput
+
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/stocks", methods=['GET', 'POST'])
+def stocks():
+    global symbolsCache
+    
+    err = None
+    chart = None
+    si = SymbolInput()
+    try:
+        si.symbols = symbolsCache
+        print("Symbols loaded from cache.")
+    except NameError:
+        print("Loading symbols cache.")
+        symbolsCache = si.loadSymbols()
+
+    form = StockForm()
+    form.setSymbolChoices(symbolsCache)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            #Get the form data to query the api
+            symbol = request.form['symbol']
+
+            if (si.isInputValid(symbol) == False):
+                err = si.error
+            
+            chart_type = request.form['chart_type']
+
+            ct = ChartTypeInput()
+            if(ct.isInputValid(chart_type) == False):
+                err = ct.error
+
+            time_series = request.form['time_series']
+
+            ts = TimeSeriesInput()
+            if(ts.isInputValid(time_series) == False):
+                err = ts.error
+
+            start_date = request.form['start_date']
+            end_date = request.form['end_date']
+
+            d = DateInput()
+            if(d.isInputValid(start_date, end_date) == False):
+                err = d.error
+
+            if err == None:
+                chart = queryStockData(symbol, chart_type, time_series, start_date, end_date)
+
+                if (chart == "throttled"):
+                    chart = None
+                    err = "We have reached our maximum call limit to Alpha Vantage. Please wait 1m and try again."
+
+            return render_template("stock.html", form=form, template="form-template", err = err, chart = chart)
+    
+    return render_template("stock.html", form=form, template="form-template")
